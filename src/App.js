a687a63ef20bcb85727d20d225b2428fe094dbdd
bhavindhodia <bhavindhodia13@gmail.com>
2021-04-28 00:02:15 +0530

    SEO Optimization

--- src/App.js
+++ src/App.js
@@ -18,6 +18,9 @@ function App() {
 const SEO=() => (
 
     <Helmet>
+    <title>Covid Dashboard</title>
+
+    {/* For Favicon */}
       <link
         rel="apple-touch-icon"
         sizes="180x180"
@@ -37,9 +40,26 @@ const SEO=() => (
       />
       <link rel="manifest" href="/favicon/site.webmanifest" />
       <link rel="mask-icon" href="/favicon/safari-pinned-tab.svg" color="#5bbad5" />
-      <meta name="msapplication-TileColor" content="#da532c" />
+      <meta name="msapplication-TileColoir" content="#da532c" />
       <meta name="theme-color" content="#ffffff" />
-      <title>Covid Dashboard</title>
+   
+   {/* Preloading */}
+    <meta name="title" content="Coronavirus Outbreak around world: Latest Map and Case Count"/>
+    <meta name="description" content="A volunteer-driven crowdsourced effort to track the coronavirus around world. A detailed country map shows the extent of the coronavirus outbreak, with data being avaliable from various date range for all countries" />
+    <meta name="keywords" content="covid, covid19, covid-19, covid19india, coronavirus, corona, india, virus, pandemic, disease, carona, karona, korona"/>
+
+    <link rel="preload" href="/fonts/archia-regular-webfont.ttf" as="font" type="font/ttf" crossorigin=""></link>
+    <link rel="preload" href="/fonts/archia-regular-webfont.woff" as="font" type="font/woff" crossorigin=""></link>
+    <link rel="preload" href="/fonts/archia-regular-webfont.woff2" as="font" type="font/woff2" crossorigin=""></link>
+
+      
+    {/* For Google Optimization */}
+    <meta property="og:type" content="website"/>
+    <meta property="og:url" content="https://bvn-covid19.netlify.app/"/>
+    <meta property="og:title" content="Coronavirus Outbreak around world: Latest Map and Case Count"></meta>
+    <meta property="og:description" content="A volunteer-driven crowdsourced effort to track the coronavirus around world. A detailed country map shows the extent of the coronavirus outbreak, with data being avaliable from various date range for all countries" />
+    <meta property="og:keywords" content="covid, covid19, covid-19, covid19india, coronavirus, corona, india, virus, pandemic, disease, carona, karona, korona" />
+
     </Helmet>
   );
 
